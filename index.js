4751a996c342c30ff7836b180b145043e3aa08f2
Philip Ford <pford68@earthlink.net>
2015-04-02 15:46:45 -0400

    Added tests

---
+++ index.js
@@ -0,0 +1,321 @@
+/**
+ *
+ * @author Philip Ford
+ */
+
+var _ = require("underscore");
+
+if (!_.isFunction(Array.from)){
+    Array.from = function($arguments, transformer){
+        var $_ = Array.prototype.slice.call($arguments);
+        if (_.isFunction(transformer)){
+            return $_.map(transformer);
+        }
+        return $_;
+    }
+}
+
+
+//================================================================ Private
+
+function Specification(spec) {
+    this.spec = spec;
+}
+_.extend(Specification.prototype, {
+    /**
+     * Returns true/false for whether the specified object has all of the properties in the component.
+     * and whether the types of the corresponding properties match.
+     *
+     * @param that
+     * @return {Boolean}
+     */
+    like: function (that) {
+        var spec = this.spec;
+        for (var i in spec) {
+            if (spec.hasOwnProperty(i) && spec[i] !== null) {
+                if (that[i] !== null && (typeof spec[i] !== typeof that[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    },
+    /**
+     * Returns true/false for whether the specs of the component and the specified object match exactly,
+     * sharing all of the same properties, and whether the types of the corresponding properties match.
+     *
+     * @param that
+     * @return {Boolean}
+     */
+    equals: function (that) {
+        var spec = this.spec;
+        for (var i in that) {
+            if (that.hasOwnProperty(i) && that[i] !== null) {
+                if (spec[i] !== null && (typeof spec[i] !== typeof that[i])) {
+                    return false;
+                }
+            }
+        }
+        return this.like(that);
+    }
+});
+
+
+//============================================================================ Public
+var ObjectDecorator = function (that) {
+    this.component = that;
+};
+
+
+Object.defineProperties(ObjectDecorator.prototype, {
+    component: {
+        enumerable: false,
+        configurable: false,
+        writable: true         // Writable defaults to false in Firefox, preventing me from writing to it later.
+    }
+});
+
+
+_.extend(ObjectDecorator.prototype, {
+
+    /**
+     * Mixes the properties of the arguments into the component.
+     *
+     * @param varargs
+     * @return {*}
+     */
+    extend: function (varargs) {
+        function _extend(a, b) {
+            for (var i in b) {
+                if (b.hasOwnProperty(i)) {
+                    a[i] = b[i];
+                }
+            }
+            return a;
+        }
+
+        var args = Array.from(arguments);
+        for (var i = 0, len = args.length; i < len; i++) {
+            _extend(this.component, args[i]);
+        }
+        return this;   // For chaining
+    },
+
+    /**
+     * Mixes the properties of the specified object into the component, but only properties that do not already
+     * exist in the component.  In other words, existing properties are not overridden in the component.
+     *
+     * @param that
+     * @return {*}
+     */
+    augment: function (that) {
+        var i, cmp = this.component;
+        for (i in that) {
+            if (that.hasOwnProperty(i))
+                if (cmp[i] == null) cmp[i] = that[i];
+        }
+        return this;   // For chaining
+    },
+
+
+    /**
+     * Adds the properties of the specified object to the component if and only if the component already
+     * has properties of the same name.
+     *
+     * @param that
+     * @return {*}
+     */
+    override: function (that) {
+        var cmp = this.component;
+        for (var i in that) {
+            if (that.hasOwnProperty(i))
+                if (i in cmp) cmp[i] = that[i];
+        }
+        return this;  // For chaining
+    },
+
+
+    /**
+     * Checks whether all of the arguments are properties of the component.
+     */
+    has: function (varargs) {
+        var cmp = this.component, args = Array.from(arguments);
+        for (var i = 1, len = args.length; i < len; ++i) {
+            if (!cmp[args[i]]) return false;
+        }
+        return true;
+    },
+
+
+    /**
+     * Performs an operation for each item in the specified object.
+     *
+     * @param callback
+     */
+    forEach: function (callback) {
+        var cmp = this.component;
+        for (var i in cmp) {
+            if (cmp.hasOwnProperty(i)) {
+                callback(cmp[i], i, cmp);
+            }
+        }
+        return this;
+    },
+
+
+    /**
+     * Creates a new object by performing a transformation on each value in the specified object.
+     * @param callback
+     */
+    map: function (callback) {
+        var result = {};
+        this.forEach(function (item, i, cmp) {
+            result[i] = callback(item, i, cmp);
+        });
+        return result;
+    },
+
+
+    /**
+     * Returns an object of component key/value pairs that passed the filter.  The filter requires a callback
+     * function that takes the following parameters:  (1) the value of the current property, the name of the
+     * current property, and the component.  That function must return true/false.
+     *
+     * @param {Function} callback
+     */
+    filter: function (callback) {
+        var result = {};
+        this.forEach(function (item, i, cmp) {
+            if (callback(item, i, cmp) === true) {
+                result[i] = item;
+            }
+        });
+        return result;
+    },
+
+
+    /**
+     * Returns the number of key/value pairs in the object.
+     *
+     * @return {Number}
+     */
+    size: function () {
+        return Object.keys(this.component).length;
+    },
+
+
+    /**
+     * Returns an object containing the differences between the component and the specified object--differences
+     * being different properties, or properties with the same name but different values.
+     *
+     * @param that
+     * @return {Object}
+     */
+    difference: function (that) {
+        var i, result = {};
+        _.extend(result, this.component);
+        for (i in that) {
+            if (that.hasOwnProperty(i)) {
+                if (result[i] && result[i] === that[i]) delete result[i];
+                else result[i] = that[i];
+            }
+        }
+        return result;
+    },
+
+
+    /**
+     * Returns an object containing the properties that match (same name and value) between the component and
+     * the specified object.
+     *
+     * @param that
+     * @return {*}
+     */
+    intersection: function (that) {
+        return this.filter(function (item, i, o) {
+            return that[i] == o[i];
+        });
+    },
+
+
+    /**
+     * Returns an array of the values in the component.
+     *
+     * @return {Array}
+     */
+    values: function () {
+        var result = [];
+        if (this.component) {
+            this.forEach(function (item) {
+                result.push(item);
+            });
+        }
+        return result;
+    },
+
+
+
+    /**
+     * Returns a "Specification" object that can be used to compare the spec for the component to
+     * the spec for another object.  Used for duck-typing.
+     */
+    getSpec: function () {
+        var spec = this.component;
+        return new Specification(spec);
+    },
+
+
+    /**
+     * Returns true/false for whether the specified value is contained in the object.
+     *
+     * @param that
+     * @return {Boolean}
+     */
+    contains: function (that) {
+        function compare(a, b) {
+            if (a && _.isFunction(a.equals)) {
+                return a.equals(b)
+            }
+            return false;
+        }
+
+        var cmp = this.component;
+
+        // Intentionally not using hasOwnProperty:
+        // I might want to know about inherited properties.
+        for (var i in cmp) {
+            if (cmp[i] == that || (compare(cmp[i], that))) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+
+    add: function (key, value) {
+        this.component[key] = value;
+        return this;
+    },
+
+    remove: function (key) {
+        var result = this.component[key];
+        delete this.component[key];
+        return result;
+    },
+
+    /**
+     * Cross-browser function for returning an object's prototype.
+     *
+     * @returns {*|Object}
+     */
+    getPrototype: function(){
+        return this.component.__proto__ || Object.getPrototypeOf(this.component);
+    }
+});
+
+
+module.exports = {
+    decorate: function (that) {
+        return new ObjectDecorator(that);
+    }
+};
---
+++ test/spec/ObjectDecoratorSpec.js
@@ -0,0 +1,417 @@
+var decorate = require("../../index").decorate;
+var _ = require("underscore");
+
+// Common data
+// Note:  when I tried to initialize this in beforeEach() below, cdata was undefined in some tests.
+var cdata = {
+    id: 'jsmith',
+    number: '34079',
+    age: 32,
+    startDate: new Date().getTime(),
+    endDate: null,
+    active: true,
+    ssn: '444-00-2222',
+    title: 'Software Developer'
+};
+
+function testIterators(item, key, scope) {
+    switch (key) {
+        case 'id':
+            expect(item).toEqual("jsmith");
+            expect(scope[key]).toEqual("jsmith");
+            break;
+        case 'age':
+            expect(item).toEqual(32);
+            expect(scope[key]).toEqual(32);
+            break;
+        case 'active':
+            expect(item).toBeTruthy();
+            expect(scope[key]).toBeTruthy();
+            break;
+        case 'title':
+            expect(item).toEqual("Software Developer");
+            expect(scope[key]).toEqual("Software Developer");
+            break;
+    }
+}
+
+
+
+describe("extend()", function () {
+
+    it("should mix the second argument into the first argument", function () {
+        var obj = {id: 3};
+        var obj2 = {author: "Philip Ford"};
+        decorate(obj).extend(obj2);
+        expect(obj.author).toBe("Philip Ford");
+        expect(obj2.id).toBeUndefined();
+    });
+
+    it("should mix all arguments, after the first argument, into the first argument", function () {
+        var obj = {id: 3};
+        var obj2 = {author: "Philip Ford"};
+        var obj3 = {copyright: "July 2012"};
+        decorate(obj).extend(obj2, obj3);
+        expect(obj.author).toBe("Philip Ford");
+        expect(obj.copyright).toBe("July 2012");
+        expect(obj2.copyright).toBeUndefined();
+    });
+
+});
+
+
+describe("augment()", function () {
+    var theObject,
+        mixin = {
+            id: 'mixin',
+            bgColor: 'red',
+            rank: '99%',
+            active: true,
+            getAge: function () {
+                return 23;
+            },
+            execute: function () {
+                return "Hello!";
+            },
+            getBgColor: function () {
+                return this.bgColor;
+            }
+        };
+
+    beforeEach(function () {
+        theObject = {
+            id: 'test',
+            active: false,
+            bgColor: null,
+            getAge: function () {
+                return 21;
+            }
+        };
+    });
+
+    it("should add new properties to an object", function () {
+        decorate(theObject).augment(mixin);
+        expect(theObject.bgColor).not.toBeNull();
+        expect(theObject.bgColor).toEqual('red');
+        expect(theObject.rank).toEqual('99%');
+        expect(theObject.getBgColor()).toEqual('red');
+        expect(theObject.execute()).toEqual('Hello!');
+    });
+
+    it("should not override existing properties, even if they are null or false", function () {
+        decorate(theObject).augment(mixin);
+        expect(theObject.id).toEqual('test');
+        expect(theObject.getAge()).toEqual(21);
+        expect(theObject.active).toBeFalsy();
+    });
+
+    it("should not remove any existing properties", function () {
+        decorate(theObject).augment(mixin);
+        expect(theObject.id).not.toBeNull();
+        expect(theObject.getAge).not.toBeNull();
+        expect(theObject.bgColor).not.toBeNull();
+        expect(theObject.active).not.toBeNull();
+
+        expect(theObject.id).toBeDefined();
+        expect(theObject.getAge).toBeDefined();
+        expect(theObject.bgColor).toBeDefined();
+        expect(theObject.active).toBeDefined();
+    });
+});
+
+
+describe("override()", function () {
+    var theObject,
+        mixin = {
+            id: 'mixin',
+            bgColor: 'red',
+            rank: '99%',
+            active: true,
+            getAge: function () {
+                return 23;
+            },
+            execute: function () {
+                return "Hello!";
+            },
+            getBgColor: function () {
+                return this.bgColor;
+            }
+        };
+
+    beforeEach(function () {
+        theObject = {
+            id: 'test',
+            active: false,
+            bgColor: null,
+            getAge: function () {
+                return 21;
+            }
+        };
+    });
+
+    it("should not add new properties an object", function () {
+        decorate(theObject).override(mixin);
+        expect(theObject.rank).not.toBeDefined();
+        expect(theObject.getBgColor).not.toBeDefined();
+        expect(theObject.execute).not.toBeDefined();
+    });
+
+    it("should override any corresponding existing properties, even if they are null or false", function () {
+        decorate(theObject).override(mixin);
+        expect(theObject.bgColor).toEqual('red');
+        expect(theObject.id).toEqual('mixin');
+        expect(theObject.getAge()).toEqual(23);
+        expect(theObject.active).toBeTruthy();
+    });
+
+    it("should not remove any existing properties", function () {
+        decorate(theObject).override(mixin);
+        expect(theObject.id).not.toBeNull();
+        expect(theObject.getAge).not.toBeNull();
+        expect(theObject.bgColor).not.toBeNull();
+        expect(theObject.active).not.toBeNull();
+
+        expect(theObject.id).toBeDefined();
+        expect(theObject.getAge).toBeDefined();
+        expect(theObject.bgColor).toBeDefined();
+        expect(theObject.active).toBeDefined();
+    });
+});
+
+
+describe("difference()", function () {
+    it("should return an object containing the differences (different key or different key/value pair)" +
+    " b/w the component and the specified object", function () {
+        var obj1 = {id: 3, name: "John"};
+        var obj2 = {id: 4, value: 45, name: "John"};
+        var diff = decorate(obj1).difference(obj2);
+        expect(diff.id).toEqual(4);
+        expect(diff.name).toBeUndefined();
+        expect(diff.value).toBe(45);
+    })
+});
+
+
+describe("size()", function () {
+    it("should return the number of properties in the specified object", function () {
+        var obj = {id: "45hgfd", name: "John", age: 26};
+        expect(decorate(obj).size()).toBe(3);
+        expect(decorate({}).size()).toBe(0);
+    });
+});
+
+describe("has()", function () {
+    var that;
+
+    beforeEach(function () {
+        that = {id: "45hgfd", name: "John", age: 26};
+    });
+
+    it("should return true if all of the arguments are keys in the component", function () {
+        expect(decorate(that).has("id", "name", "age")).toBeTruthy();
+    });
+
+    it("should return false if any of the arguments is not a key in the component", function () {
+        expect(decorate({}).has("id", "name", "age")).toBeFalsy();
+        expect(decorate({id: 'jsmith'}).has("id", "name", "age")).toBeFalsy();
+        expect(decorate({id: 'pjones', age: 44}).has("id", "name", "age")).toBeFalsy();
+        expect(decorate({id: 'pjones', name: 'Pete Jones'}).has("id", "name", "age")).toBeFalsy();
+    });
+});
+
+
+describe("forEach()", function () {
+    it("should perform the specified action for each item in the component", function () {
+        var items = [], nulls = [];
+        decorate(cdata).forEach(function (item, key, scope) {
+            items.push(item);
+            if (item === null) {
+                nulls.push(key);
+            }
+        });
+        expect(items.length).toEqual(8);
+        expect(nulls.length).toEqual(1);
+        expect(nulls[0]).toEqual("endDate");
+    });
+
+    it("should make the each item, key, and the component available to the callback, in that order", function () {
+        decorate(cdata).forEach(function (item, key, scope) {
+            testIterators(item, key, scope);
+        });
+    });
+});
+
+
+describe("map()", function () {
+    var a = decorate(cdata).map(function (item) {
+        return false;
+    });
+    var b = decorate(cdata).map(function (item) {
+        return true;
+    });
+    var c = decorate(cdata).map(function (item, key) {
+        if (key === "ssn") return "XXX-XX-XXXX";
+        else if (key === "active") return false;
+        else return item += "!";
+    });
+
+
+    it("should return an Object", function () {
+        expect(a.constructor).toBe(Object);
+        expect(b.constructor).toBe(Object);
+    });
+
+    it("should transform the component values, using the callback, and return the transformed values in a new object", function () {
+        expect(c.age).toEqual("32!");
+        expect(c.active).toBeFalsy();
+        expect(c.id).toEqual('jsmith!');
+        expect(c.title).toEqual('Software Developer!');
+        expect(c.number).toEqual("34079!");
+        expect(c.ssn).toEqual("XXX-XX-XXXX");
+    });
+
+    it("should make the each item, key, and the component available to the callback, in that order", function () {
+        decorate(cdata).map(function (item, key, scope) {
+            testIterators(item, key, scope);
+        });
+    });
+});
+
+
+describe("filter()", function () {
+    var a = decorate(cdata).filter(function (item) {
+        return false;
+    });
+    var b = decorate(cdata).filter(function (item) {
+        return true;
+    });
+    var c = decorate(cdata).filter(function (item, key) {
+        return ( typeof item === 'string');
+    });
+
+    it("should return an Object", function () {
+        expect(a.constructor).toBe(Object);
+        expect(b.constructor).toBe(Object);
+    });
+
+    it("should return a new object containing only the component properties that pass the filter", function () {
+        expect(c.age).toBeUndefined();
+        expect(c.active).toBeUndefined();
+        expect(c.id).toEqual('jsmith');
+        expect(c.title).toEqual('Software Developer');
+        expect(c.number).toEqual("34079");
+        expect(c.ssn).toEqual("444-00-2222");
+    });
+
+    it("should make the each item, key, and the component available to the callback, in that order", function () {
+        decorate(cdata).forEach(function (item, key, scope) {
+            testIterators(item, key, scope);
+        });
+    });
+});
+
+
+describe("intersection()", function () {
+    it("should return a new object containing only the similarities (same property name and value) between the component and specified object", function () {
+        var obj1 = {id: 3, name: "John"};
+        var obj2 = {id: 4, value: 45, name: "John"};
+        var is = decorate(obj1).intersection(obj2);
+        expect(is.id).toBeUndefined();
+        expect(is.name).toBe("John");
+        expect(is.value).toBeUndefined();
+    })
+});
+
+
+describe("values()", function () {
+    var values = decorate(cdata).values();
+    it("should return an array of the values of the component's properties", function () {
+        expect(values.length).toEqual(8);
+        expect(values[0]).toEqual("jsmith");
+        expect(values[7]).toEqual("Software Developer");
+    });
+
+    it("should return [] for null, undefined, and {}", function () {
+        try {
+            expect(decorate({}).values().length).toEqual(0);
+            expect(decorate(null).values().length).toEqual(0);
+            expect(decorate(undefined).values().length).toEqual(0);
+        } catch (e) {
+            this.fail("We should not reach this point.");
+        }
+    });
+});
+
+
+describe("getSpec()", function () {
+
+    var that = {
+        id: "pjones",
+        age: 45,
+        number: '44566',
+        title: "Software Developer",
+        active: true,
+        startDate: new Date().getTime(),
+        endDate: new Date().getTime(),
+        ssn: "444-11-5555"
+    };
+
+
+    describe("like()", function () {
+        it("should return true if the specified object has all of the same properties, with the same data type, as the component", function () {
+            expect(decorate(cdata).getSpec().like(that)).toBeTruthy();
+        });
+
+        it("should be irrelevant that the component does not have all of the properties present in the specified object", function () {
+            var obj = {};
+            _.extend(obj, that);
+            obj.favorites = ['skiiing'];
+            expect(decorate(cdata).getSpec().like(obj)).toBeTruthy();
+        });
+
+        it("should be still return true if one of the corresponding properties is null", function () {
+            var obj = {};
+            _.extend(obj, that);
+            obj.id = null;
+            expect(decorate(cdata).getSpec().like(obj)).toBeTruthy();
+        });
+
+        it("should return false if the specified object does not have one or more properties present in the component", function () {
+            var obj = {};
+            _.extend(obj, that);
+            delete obj.ssn;
+            expect(decorate(cdata).getSpec().like(obj)).toBeFalsy();
+        });
+    });
+
+    describe("equals()", function () {
+        it("should return true if the specified object and the component have exactly the same property names, with the same data types", function () {
+            var obj = {};
+            _.extend(obj, that);
+            delete obj.ssn;
+            expect(decorate(cdata).getSpec().equals(that)).toBeTruthy();
+            expect(decorate(cdata).getSpec().equals(obj)).toBeFalsy();
+        });
+
+        it("should return false if the specified object does not have one or more properties present in the component", function () {
+            var obj = {};
+            _.extend(obj, that);
+            delete obj.ssn;
+            expect(decorate(cdata).getSpec().equals(obj)).toBeFalsy();
+        });
+
+        it("should return false if the component does not have all of the properties present in the specified object", function () {
+            var obj = {};
+            _.extend(obj, that);
+            obj.favorites = ['skiiing'];
+            expect(decorate(cdata).getSpec().equals(obj)).toBeFalsy();
+        });
+
+        it("should be still return true if one of the corresponding properties is null", function () {
+            var obj = {};
+            _.extend(obj, that);
+            obj.id = null;
+            expect(decorate(cdata).getSpec().equals(obj)).toBeTruthy();
+        });
+    });
+});
